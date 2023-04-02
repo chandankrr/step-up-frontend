40b5f643f7dd63032bed85c68c40f0102ed7be2d
Chandan Kumar <chandankrr.91@gmail.com>
2023-04-02 18:29:39 +0530

    fetched data for category menu dynamically

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,6 +6,7 @@ import Menu from './Menu';
 import MenuMobile from './MenuMobile';
 import Wrapper from './Wrapper';
 
+import { fetchDataFromApi } from '@/utils/api';
 import { BiMenuAltRight } from 'react-icons/bi';
 import { BsCart } from 'react-icons/bs';
 import { IoMdHeartEmpty } from 'react-icons/io';
@@ -16,6 +17,7 @@ const Header = () => {
   const [showCatMenu, setShowCatMenu] = useState(false);
   const [show, setShow] = useState('translate-y-0');
   const [lastScrollY, setLastScrollY] = useState(0);
+  const [categories, setCategories] = useState(null);
 
   const controlNavbar = () => {
     if (window.scrollY > 200) {
@@ -37,6 +39,15 @@ const Header = () => {
     };
   }, [lastScrollY]);
 
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const fetchCategories = async () => {
+    const { data } = await fetchDataFromApi('/api/categories?populate=*');
+    setCategories(data);
+  };
+
   return (
     <header
       className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
@@ -46,13 +57,18 @@ const Header = () => {
           <img src="/logo.svg" alt="logo" className="w-[40px] md:w-[60px]" />
         </Link>
 
-        <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} />
+        <Menu
+          showCatMenu={showCatMenu}
+          setShowCatMenu={setShowCatMenu}
+          categories={categories}
+        />
 
         {mobileMenu && (
           <MenuMobile
             showCatMenu={showCatMenu}
             setShowCatMenu={setShowCatMenu}
             setMobileMenu={setMobileMenu}
+            categories={categories}
           />
         )}
 
